c84235e2dc131f3ffd8fd6bec22796fa02e273e6
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2009-10-11 07:06:43 +0000

    Backported the code that fixed #47.

--- pykota/version.py
+++ pykota/version.py
@@ -13,7 +13,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -23,7 +23,7 @@
 
 """This module defines some application level constants."""
 
-__version__ = "1.26_unofficial"
+__version__ = "1.26_fixes_unofficial"
 
 __doc__ = "PyKota : a complete Printing Quota Solution for CUPS."
 
